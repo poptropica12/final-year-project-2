4ef644a560d2ae2d7c8a096a517ce82e73cae292
poptropica12 <yncheng22-c@my.cityu.edu.hk>
2019-05-29 00:14:53 +0800

    better

--- base.js
+++ base.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection ({
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'customer'
 });
 conn.connect();
